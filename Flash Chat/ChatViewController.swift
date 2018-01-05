10d85088d4a375a0d51ee4ee362fce6de1850f9d
Somya <somya.nalco@hotmail.com>
2018-01-05 05:55:03 +0530

    1. Manage textField(Active, Deactive) 2. Send Button(Active, Deactive) 3. Create a child dataBase. 4. Send the message to fireBase child dataBase.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -126,7 +126,24 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBAction func sendPressed(_ sender: AnyObject) {
         
         
-        //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.endEditing(true)
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messageDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextfield.text!]
+        messageDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Message saved sucessfully")
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+            }
+        }
         
         
     }
